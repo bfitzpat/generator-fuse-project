b05aacc4f9559a45b66b6757c824ad61788351a6
Brian Fitzpatrick <bfitzpat@redhat.com>
2018-08-14 15:01:26 -0600

    fixing mocha tests for spring and blueprint variants

--- test/app.js
+++ test/app.js
@@ -3,34 +3,35 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 var basicProps = {};
+var fs = require('fs-extra');
 
 describe('generator-camel:app', function () {
 
-  before(function () {
-    basicProps.name = 'MyAppMock';
-    basicProps.package = 'com.generator.mock';
-    basicProps.camelVersion = '2.18.2'
-    basicProps.camelDSL = 'spring'
-  });
-
-  describe('Should properly scaffold with default config', function () {
+  describe('Should properly scaffold with default config for Spring', function () {
 
     before(function () {
-      return helpers.run(path.join(__dirname, '../generators/app'))
+      basicProps.name = 'MyAppMock';
+      basicProps.package = 'com.generator.mock';
+      basicProps.camelVersion = '2.18.2';
+      basicProps.camelDSL = 'spring';
+
+      return helpers.run(path.join(__dirname, '../app'))
+        .inTmpDir(function (dir) {
+          var done = this.async(); // `this` is the RunContext object.
+          fs.copy(path.join(__dirname, '../fuse-project'), dir, done);
+        })
         .withPrompts({ name: basicProps.name })
         .withPrompts({ camelVersion: basicProps.camelVersion })
-        .withPrompts({ release: basicProps.camelDSL })
+        .withPrompts({ camelDSL: basicProps.camelDSL })
         .withPrompts({ package: basicProps.package })
-        .toPromise();
+        .toPromise(); //.
     });
 
     it('Should create the basic structure', function () {
-      assert.file([
-        '.gitignore',
-        'pom.xml',
-        'README.md',
-        'src/main/resources/META-INF/spring/camel-context.xml',
-      ]);
+      assert.file('.gitignore');
+      assert.file('pom.xml');
+      assert.file('README.md');
+      assert.file('src/main/resources/META-INF/spring/camel-context.xml');
     });
 
     it('Should create pom.xml with default content', function () {
@@ -38,4 +39,38 @@ describe('generator-camel:app', function () {
       assert.fileContent('pom.xml', new RegExp('<artifactId>' + basicProps.name + '</artifactId>') );
     });
   });
+
+  describe('Should properly scaffold with default config for Blueprint', function () {
+
+    before(function () {
+      basicProps.name = 'MyAppMockBP';
+      basicProps.package = 'com.generator.mock.bp';
+      basicProps.camelVersion = '2.18.2';
+      basicProps.camelDSL = 'blueprint';
+
+      return helpers.run(path.join(__dirname, '../app'))
+        .inTmpDir(function (dir) {
+          var done = this.async(); // `this` is the RunContext object.
+          fs.copy(path.join(__dirname, '../fuse-project'), dir, done);
+        })
+        .withPrompts({ name: basicProps.name })
+        .withPrompts({ camelVersion: basicProps.camelVersion })
+        .withPrompts({ camelDSL: basicProps.camelDSL })
+        .withPrompts({ package: basicProps.package })
+        .toPromise(); //.
+    });
+
+    it('Should create the basic structure', function () {
+      assert.file('.gitignore');
+      assert.file('pom.xml');
+      assert.file('README.md');
+      assert.file('src/main/resources/OSGI-INF/blueprint/blueprint.xml');
+    });
+
+    it('Should create pom.xml with default content', function () {
+      assert.fileContent('pom.xml', new RegExp('<groupId>' + basicProps.package + '</groupId>') );
+      assert.fileContent('pom.xml', new RegExp('<artifactId>' + basicProps.name + '</artifactId>') );
+    });
+  });
+
 });
